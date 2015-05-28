5a2eb0636acba3778de8aa6dd238fb884cce4dea
Mat Harden <matharden@gmail.com>
2015-05-28 15:43:47 +0100

    WIP: using Mojular internal template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  layout 'erb/base'
-
   bowerrc_dir = JSON.parse(IO.read("#{Rails.root.to_s}/.bowerrc"))['directory']
   prepend_view_path(File.expand_path("#{Rails.root.to_s}/#{bowerrc_dir}/mojular"))
 end
