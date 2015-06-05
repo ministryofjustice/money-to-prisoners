3db4d11f0b3aeeeb8a071673c05a7d1f8301416c
Todd Tyree <tatyree@gmail.com>
2015-06-05 11:37:02 +0100

    Remove application_controller boilerplate
    Left in as part of the initial commit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   bowerrc_dir = JSON.parse(IO.read("#{Rails.root.to_s}/.bowerrc"))['directory']
