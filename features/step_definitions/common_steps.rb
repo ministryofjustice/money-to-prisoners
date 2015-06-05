327647f96d6161261c0af4271d210eaa8b13304b
Todd Tyree <tatyree@gmail.com>
2015-06-05 17:22:00 +0100

    Store created objects into generic instances vars
    This is to make it easier to look them the page at a later time  I also
    create a singluar version of the instance var if the user only asks for
    a single object.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,5 +1,6 @@
 Given(/^(\d+) (\w+) exist$/) do |count, klass|
-  create_list(klass.singularize.to_sym, count.to_i)
+  @objekts = create_list(klass.singularize.to_sym, count.to_i)
+  @objekt  = @objekts.first if count.to_i == 1
 end
 
 Then(/^I see (\d+) (\w+)$/) do |count, klass|
