03ee16343dec879f6d6b154dd16c1d24b08a8388
Todd Tyree <tatyree@gmail.com>
2015-06-05 16:42:23 +0100

    Add a generic css class lookup step
    This is a very high level step that looks for a number of occurrences of
    a specified class on a page.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,3 +1,7 @@
 Given(/^(\d+) (\w+) exist$/) do |count, klass|
   create_list(klass.singularize.to_sym, count.to_i)
 end
+
+Then(/^I see (\d+) (\w+)$/) do |count, klass|
+  expect(page).to have_selector(".#{klass.singularize}", count: count.to_i)
+end
